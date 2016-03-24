375a188f3f471a34717dad136dd06149b9577b1a
LindaRNguyen <lindar.nguyen@mail.utoronto.ca>
2016-03-24 12:12:13 -0400

    content.php fix
    i swear i already updated so many times already why is it getting undone
    again

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,6 +17,7 @@
 				<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark">
 				<?php the_title(); ?>
 				</a></h2>
+				
 				<?php the_content();
 			} else {
 				?>
